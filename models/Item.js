1a0df9b04e70e27504e6fb804ab87539a9e0d9a6
Shixin Yang <shixiny@sfu.ca>
2019-11-14 20:49:33 -0800

    Implemented items model and routes.

---
+++ models/Item.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    brand: {
+        name: {
+            type: String,
+            required: true
+        },
+        popularity: {
+            type: Boolean,
+            required: true
+        }
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    details: {
+        type: String
+    },
+
+    isOnSale: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+
+    isAvailable: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+
+    remaining: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('item', ItemSchema);
--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 
-//@route        POST api/items
+const Item = require("../models/Item");
+//@route        Get api/items
 //@desc         Get all items
 //@access       Public
-router.get('/', (req, res) => {
-    res.send("Get all items")
+router.get('/', async (req, res) => {
+    try {
+        let items = await Item.find();
+        if (items) {
+            res.json(items);
+        }
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error.")
+    }
+})
+
+
+
+//@route  POST api/items
+router.post('/', async (req, res) => {
+
+    const items = req.body;
+    items.map(async item => {
+        try {
+            const newTtem = new Item(item)
+            const what = await newTtem.save();
+            res.json(what)
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error.")
+        }
+    })
+
 })
 
 
