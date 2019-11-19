6335d13fcd1010ec5072568a6ccea2c7412da1a0
Shixin Yang <shixiny@sfu.ca>
2019-11-18 17:39:34 -0800

    enable all requests to support CORS

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.get('/', (req, res) => {
 
 
 //define all routes
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/api/users', require('./routes/users'));
 app.use('/api/items', require('./routes/items'));
 app.use('/api/auth', require('./routes/auth'));
