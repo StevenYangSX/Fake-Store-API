aa7e5198a0cc8493a11f23c5a77e1a88496913c5
Shixin Yang <shixiny@sfu.ca>
2019-11-20 23:12:03 -0800

    Added documnent file for API loading page.

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express = require('express');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const connectDB = require("./config/db");
+var path = require('path');
 //initialize app
 const app = express();
 //connect to database
-connectDB()
-
+connectDB();
 
 //init middleware
-app.use(express.json({
-    extended: false
-}))
+app.use(
+    express.json({
+        extended: false
+    })
+);
 //add an endpoint
-app.get('/', (req, res) => {
-    res.send(res.json({
-        msg: "Welcome to the fake store API"
-    }))
-})
-
+app.get("/api", (req, res) => {
+    // res.send(res.json({
+    //     msg: "Welcome to the fake store API"
+    // }));
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 //define all routes
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
 });
-app.use('/api/users', require('./routes/users'));
-app.use('/api/items', require('./routes/items'));
-app.use('/api/auth', require('./routes/auth'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/items", require("./routes/items"));
+app.use("/api/auth", require("./routes/auth"));
 
 //set port
 const PORT = process.env.PORT || 5000;
